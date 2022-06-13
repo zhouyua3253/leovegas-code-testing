7eb5731b012e623a1ff784682c744fb92c05483a
Yumin <yumin.zhou@hotmail.com>
2022-06-13 23:14:48 +0200

    npm install storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,21 @@
+import "../styles/globals.css";
+import * as NextImage from "next/image";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+/**
+ * Use the unoptimized prop for Next.js Images in Storybook
+ */
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
