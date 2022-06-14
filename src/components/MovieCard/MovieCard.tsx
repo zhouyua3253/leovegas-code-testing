59c8489414fb49e3fbe3e94b610445ef46d0e522
Yumin <yumin.zhou@hotmail.com>
2022-06-14 16:21:21 +0200

    Implement PosterImage component.

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,8 @@
 import React, { HTMLAttributes } from "react";
 import { MovieModel } from "@Models/movieModel";
 import clsx from "clsx";
+import PosterImage from "@Components/PosterImage";
+import dayjs from "dayjs";
 
 export interface MovieCardProps extends HTMLAttributes<HTMLDivElement> {
   /**
@@ -21,15 +23,22 @@ export default function MovieCard({
   as = "div",
   ...rest
 }: MovieCardProps) {
+  const { poster_path, title, release_date } = movie;
+  const formattedDate = dayjs(release_date).format("DD MMM YYYY");
+
   const Container = as;
 
   return (
     <Container className={clsx("max-w-screen-sm", className)} {...rest}>
-      <p className="text-base" aria-label={movie.original_title}>
-        {movie.original_title}
-      </p>
-      <p className="text-sm" aria-label={movie.overview}>
-        {movie.overview}
+      {poster_path && (
+        <PosterImage src={poster_path} alt={title} className="rounded-xl" />
+      )}
+
+      <h4 className="text-base mt-2 mb-1 font-semibold" aria-label={title}>
+        {title}
+      </h4>
+      <p className="text-sm text-gray-500" aria-label={formattedDate}>
+        {formattedDate}
       </p>
     </Container>
   );
