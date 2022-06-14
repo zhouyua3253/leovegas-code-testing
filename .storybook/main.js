138a0095101c7285957fbcd125f988bfafcf0140
Yumin <yumin.zhou@hotmail.com>
2022-06-14 15:51:24 +0200

    Properly setup tailwindcss for storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,18 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
