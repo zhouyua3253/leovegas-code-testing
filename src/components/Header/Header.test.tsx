5d86d77722444c12cb3758e71eb76507bcfba5ef
Yumin <yumin.zhou@hotmail.com>
2022-06-16 15:36:43 +0200

    Implement the page Header component.

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Header from "./Header";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
-describe("Components/Select", () => {
-  it('Should use "header" tag', () => {
+describe("Components/Header", () => {
+  it('Should use "header" tag as container.', () => {
     render(<Header />);
 
-    expect(document.querySelector("header")).toBeInTheDocument();
+    expect(document.querySelector("header")).toBeVisible();
+  });
+
+  it("Should have a link to home page.", () => {
+    render(<Header />);
+
+    const homePageLink = screen.getByTestId("home-page-link");
+    expect(homePageLink).toHaveAttribute("href", "/");
+    expect(homePageLink).toBeVisible();
+  });
+
+  it("Should have a link to watch later page.", () => {
+    render(<Header />);
+
+    const watchLaterPageLink = screen.getByTestId("watch-later-page-link");
+    expect(watchLaterPageLink).toHaveAttribute("href", "/watch-later");
+    expect(watchLaterPageLink).toBeVisible();
   });
 });
---
+++ src/pages/watch-later.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+export default function WatchLaterPage() {
+  return (
+    <>
+      <Head>
+        <title>Movie Gallery</title>
+        <meta name="description" content="Movie Gallery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>WATCH LATER</div>
+    </>
+  );
+}
